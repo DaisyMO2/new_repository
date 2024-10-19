2515cf6dbd531f88b65df042c0ea10d95b66d999
DaisyMO2 <marungadaizy10@gmail.com>
2024-10-19 08:12:48 +0100

    Add files via upload
    practice for probability

---
+++ probability practice/first.py
@@ -0,0 +1,28 @@
+## single line comment
+'''
+multi line comment
+'''
+
+## declaring variables
+name = 'daisy'
+number_one = 12
+number_two = 3.14
+status = False
+
+
+print(number_one+number_two)
+print(number_one +number_two)
+
+#declaring a function
+def subtraction():
+    defference = number_one - number_two
+    return defference
+
+print(subtraction())
+
+## a void function
+def addition():
+    sum = number_one + number_two
+    print(sum)
+
+addition()
